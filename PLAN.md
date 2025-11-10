60efbdbd6a8bac8f44ab7eefc3b2f7dc4cfa20a0
Andrew Richmond <41588533+acrmine@users.noreply.github.com>
2025-11-09 16:34:16 -0800

    Changed the concept of token class to a type so that the map can store a lightweight reference to each token and then handle interaction itself, checked off token type and map class in PLAN.md

--- PLAN.md
+++ PLAN.md
@@ -1,16 +1,16 @@
 # D3: Token Merging
 
-# Game Design Vision
+## Game Design Vision
 
 A website that tracks your location if you use it on your phone to navigate a map of the world (can also navigate using controls and not a map). You can pick up tokens in various locations and keep them with you before combining them with other tokens you find in other squares.
 
-# Technologies
+## Technologies
 
 - TypeScript for most game code, little to no explicit HTML, and all CSS collected in common `style.css` file
 - Deno and Vite for building
 - GitHub Actions + GitHub Pages for deployment automation
 
-# Assignments
+## Assignments
 
 ## D3.a: Core mechanics (token collection and crafting)
 
@@ -25,7 +25,8 @@ Key gameplay challenge: Can players collect and craft tokens from nearby locatio
 - [x] draw the player's location on the map
 - [x] draw a rectangle representing one cell on the map
 - [x] use loops to draw a whole grid of cells on the map
-- [ ] make a token class that can be clicked on
+- [x] make a token type that can store a location and value reference
+- [x] make a map class that stores token locations and leaflet map
 - [ ] change spawn cache to spawn a token in a given cell (can also spawn the rectangle for now)
 - [ ] allow tokens clicked on within range to be displayed in an inventory under the map
 - [ ] allow tokens to be placed back down again when you click on an empty cell
--- src/main.ts
+++ src/main.ts
@@ -11,11 +11,6 @@ import "./_leafletWorkaround.ts"; // fixes for missing Leaflet images
 // Import our luck function
 import luck from "./_luck.ts";
 
-// Create basic UI elements
-const mapDiv = document.createElement("div");
-mapDiv.id = "map";
-document.body.append(mapDiv);
-
 // Our classroom location
 const CLASSROOM_LATLNG = leaflet.latLng(
   36.997936938057016,
@@ -28,47 +23,69 @@ const TILE_DEGREES = 1e-4;
 const NEIGHBORHOOD_SIZE = 8;
 const CACHE_SPAWN_PROBABILITY = 0.1;
 
-// Create the map (element with id "map" is defined in index.html)
-const map = leaflet.map(mapDiv, {
-  center: CLASSROOM_LATLNG,
-  zoom: GAMEPLAY_ZOOM_LEVEL,
-  minZoom: GAMEPLAY_ZOOM_LEVEL,
-  maxZoom: GAMEPLAY_ZOOM_LEVEL,
-  zoomControl: false,
-  scrollWheelZoom: false,
-});
+type Token = {
+  x: number;
+  y: number;
+  value: number;
+};
 
-// Populate the map with a background tile layer
-leaflet.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  maxZoom: 19,
-  attribution:
-    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-}).addTo(map);
+class Map {
+  obj: leaflet.Map;
+  tokens: Token[] = [];
+
+  constructor(center: leaflet.LatLng, zoom: number) {
+    const mapDiv = document.createElement("div");
+    mapDiv.id = "map";
+    document.body.append(mapDiv);
+
+    this.obj = leaflet.map(mapDiv, {
+      center: center,
+      zoom: zoom,
+      minZoom: zoom,
+      maxZoom: zoom,
+      zoomControl: false,
+      scrollWheelZoom: false,
+    });
+
+    // Populate the map with a background tile layer
+    leaflet.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      maxZoom: 19,
+      attribution:
+        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    }).addTo(this.obj);
+  }
+
+  spawnCache(i: number, j: number) {
+    // Convert cell numbers into lat/lng bounds
+    const origin = CLASSROOM_LATLNG;
+    const bounds = leaflet.latLngBounds([
+      [origin.lat + i * TILE_DEGREES, origin.lng + j * TILE_DEGREES],
+      [
+        origin.lat + (i + 1) * TILE_DEGREES,
+        origin.lng + (j + 1) * TILE_DEGREES,
+      ],
+    ]);
+
+    // Add a rectangle to the map to represent the cache
+    const rect = leaflet.rectangle(bounds);
+    rect.addTo(this.obj);
+  }
+}
+
+// Create the map centered on the classroom
+const map = new Map(CLASSROOM_LATLNG, GAMEPLAY_ZOOM_LEVEL);
 
 // Add a marker to represent the player
 const playerMarker = leaflet.marker(CLASSROOM_LATLNG);
 playerMarker.bindTooltip("That's you!");
-playerMarker.addTo(map);
-
-function spawnCache(i: number, j: number) {
-  // Convert cell numbers into lat/lng bounds
-  const origin = CLASSROOM_LATLNG;
-  const bounds = leaflet.latLngBounds([
-    [origin.lat + i * TILE_DEGREES, origin.lng + j * TILE_DEGREES],
-    [origin.lat + (i + 1) * TILE_DEGREES, origin.lng + (j + 1) * TILE_DEGREES],
-  ]);
-
-  // Add a rectangle to the map to represent the cache
-  const rect = leaflet.rectangle(bounds);
-  rect.addTo(map);
-}
+playerMarker.addTo(map.obj);
 
 // Look around the player's neighborhood for caches to spawn
 for (let i = -NEIGHBORHOOD_SIZE; i < NEIGHBORHOOD_SIZE; i++) {
   for (let j = -NEIGHBORHOOD_SIZE; j < NEIGHBORHOOD_SIZE; j++) {
     // If location i,j is lucky enough, spawn a cache!
     if (luck([i, j].toString()) < CACHE_SPAWN_PROBABILITY) {
-      spawnCache(i, j);
+      map.spawnCache(i, j);
     }
   }
 }
